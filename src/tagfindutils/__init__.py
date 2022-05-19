fa95ed9c7edc541ea494fb9f013db223fa7dd7e5
nukemiko <north666dakota@gmail.com>
2022-05-19 11:48:56 +0800

    增加了一个函数，用于查找支持的歌曲信息搜索来源和对应的搜索入口

--- src/tagfindutils/__init__.py
+++ src/tagfindutils/__init__.py
@@ -1,1 +1,26 @@
 from __future__ import annotations
+
+from typing import Any, Callable, List, Union
+
+from . import cloudmusic
+from . import qqmusic
+from .structures import SearchResult
+
+SearchFunc = Union[
+    Callable[[Any], List[cloudmusic.CloudMusicSearchResult]],
+    Callable[[Any], List[qqmusic.QQMusicSearchResult]]
+]
+
+
+def supported_sources() -> dict[str, SearchFunc]:
+    """列出支持的歌曲信息搜索来源，以及对应的搜索入口函数。
+
+    目前支持的搜索来源：
+
+    - cloudmusic - 网易云音乐
+    - qqmusic - QQ 音乐
+    """
+    return {
+        'cloudmusic': cloudmusic.search,
+        'qqmusic': qqmusic.search
+    }
