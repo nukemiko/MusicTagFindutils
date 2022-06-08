02f115234247509ddcb8cd2a9e3e280aea6cdb84
nukemiko <north666dakota@gmail.com>
2022-06-08 18:34:56 +0800

    删除了 supported_sources() 的类型注解

--- src/tagfindutils/__init__.py
+++ src/tagfindutils/__init__.py
@@ -1,20 +1,13 @@
 from __future__ import annotations
 
-from typing import Any, Callable, List, Union
-
 from . import cloudmusic
 from . import qqmusic
 from .structures import SearchResult
 
 __VERSION__ = '0.1.1'
 
-SearchFunc = Union[
-    Callable[[Any], List[cloudmusic.CloudMusicSearchResult]],
-    Callable[[Any], List[qqmusic.QQMusicSearchResult]]
-]
 
-
-def supported_sources() -> dict[str, SearchFunc]:
+def supported_sources():
     """列出支持的歌曲信息搜索来源，以及对应的搜索入口函数。
 
     目前支持的搜索来源：
